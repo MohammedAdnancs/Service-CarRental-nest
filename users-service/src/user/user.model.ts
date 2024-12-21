5195097d8be69b5a46f2ebd06f5511ebd365d9be
MohammedAdnan <MohammedAdnan.cs@hotmail.com>
2024-12-21 14:06:00 +0200

    user and authentication

---
+++ users-service/src/user/user.model.ts
@@ -0,0 +1,14 @@
+/* eslint-disable prettier/prettier */
+import { Schema, Document, model } from 'mongoose';
+
+export interface User extends Document {
+  readonly username: string;
+  readonly password: string;
+}
+
+export const UserSchema = new Schema({
+  username: { type: String, required: true },
+  password: { type: String, required: true },
+});
+
+export const UserModel = model<User>('UserSchema', UserSchema);
---
+++ users-service/src/user/user.module.ts
@@ -0,0 +1,11 @@
+/* eslint-disable prettier/prettier */
+import { Module } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserController } from './user.controller';
+import { HttpModule } from '@nestjs/axios';
+@Module({
+  imports: [HttpModule],
+  providers: [UserService],
+  controllers: [UserController]
+})
+export class UserModule {}
