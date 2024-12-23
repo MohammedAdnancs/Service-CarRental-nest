95a1c136d67244892c110edb8e868d3753137ad9
AndrewHabel <157292743+AndrewHabel@users.noreply.github.com>
2024-12-23 20:03:25 +0200

    Update user.model.ts

--- users-service/src/user/user.model.ts
+++ users-service/src/user/user.model.ts
@@ -7,7 +7,7 @@ export interface User extends Document {
 }
 
 export const UserSchema = new Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   password: { type: String, required: true },
 });
 
